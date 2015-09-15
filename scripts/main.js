7c1c348101e62d2609655a74a59eabb2c8ffcde0
Mike Mathew <m2mathew@me.com>
2015-09-14 22:40:09 -0500

    Loading button finalized on normal mode

--- scripts/main.js
+++ scripts/main.js
@@ -17,35 +17,3 @@ function originalButton() {
 
 // add event listener
 $loadingButton.on('click', loadingTime);
-
-// $(function() {
-//     $("#progressbar").progressbar({ value: 0 });
-//     setTimeout(updateProgress, 500);
-//     });
-
-// function updateProgress() {
-//   var progress;
-//   progress = $("#progressbar")
-//     .progressbar("option","value");
-//   if (progress < 100) {
-//       $("#progressbar")
-//         .progressbar("option", "value", progress + 1);
-//       setTimeout(updateProgress, 500);
-//   }
-// }
-
-function makeMoney() {
-        $("#makeMoney").attr('disabled', 'disabled');
-        fillAgain();
-    }
-
-    function fillAgain() {
-        setTimeout(function ()
-        {
-            if($("#progressbar").find('div').length <= 10)
-            {
-                $("#divfillmeUp").append("<div style='background-color: red; width: 10%; height: 1%;float: left'></div>");
-                fillAgain();
-            }
-        },2000)
-    }
